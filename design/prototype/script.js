78e7f4a95b13b00dfc1cf310167f17625d10471c
nikhilwebdeveloper <108711205+nikhilwebdeveloper@users.noreply.github.com>
2026-07-17 08:45:21 -0700

    Update script.js

--- design/prototype/script.js
+++ design/prototype/script.js
@@ -1,1 +1,19 @@
+function updateClock(){
 
+    const now=new Date();
+
+    const time=now.toLocaleTimeString([],{
+
+        hour:"2-digit",
+
+        minute:"2-digit"
+
+    });
+
+    document.getElementById("clock").textContent=time;
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
